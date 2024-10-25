220948c22f17552e97ba4c17972b4ec84a24fcc0
Prashant Kumar <tumsab7777@gmail.com>
2024-10-25 12:03:05 +0530

    v1.5 clean-up

--- src/index.ts
+++ src/index.ts
@@ -208,208 +208,7 @@ app.post("/order/buy" , async(req : Request , res : Response)=>{
     const body = await req.body
     const stockType : "yes" | "no" = body.stockType
 
-    // const currentBalance = INR_BALANCES[body.userId].balance
-    // const orderBookData = ORDERBOOK[body.stockSymbol][stockType]
-    // const totalAmount = body.quantity*body.price
-    // const currentData = STOCK_BALANCES[body.userId]
-
-    // // this case is possible when buy order exist as it in the sell order and exchange happens
-    // if(currentBalance >= totalAmount){
-    //     var lockedAmount  = 0;
-    //     INR_BALANCES[body.userId].balance -= totalAmount
-    //     INR_BALANCES[body.userId].locked = totalAmount
-    //     lockedAmount = lockedAmount + INR_BALANCES[body.userId].locked
-    //     // when quantity of the supply is same as demand for stock
-
-    //     // if(lockedAmount === 1000){}
-
-    //     // case 1: have to mint the whole req amount of stocks
-    //     if(!orderBookData[body.price]){
-    //         const amountToPlaceSell = 1000*body.quantity - body.price*body.quantity
-    //         const sellingId = `probo_${body.userId}`
-
-
-
-    //         if(stockType === "yes"){
-    //             ORDERBOOK[body.stockSymbol]["no"][amountToPlaceSell/body.quantity] =  {
-    //                 total : body.quantity + ORDERBOOK[body.stockSymbol]["no"][amountToPlaceSell/body.quantity].total,
-    //                 orders : {
-    //                     ...ORDERBOOK[body.stockSymbol]["no"][amountToPlaceSell/body.quantity].orders,
-    //                    [sellingId] : body.quantity 
-    //                 }
-    //             }
-    //         } else if(stockType === "no"){
-    //             ORDERBOOK[body.stockSymbol]["yes"][amountToPlaceSell/body.quantity] =  {
-    //                 total : body.quantity + ORDERBOOK[body.stockSymbol]["yes"][amountToPlaceSell/body.quantity].total,
-    //                 orders : {
-    //                     ...ORDERBOOK[body.stockSymbol]["yes"][amountToPlaceSell/body.quantity].orders,
-    //                    [sellingId] : body.quantity
-    //                 }
-    //             }
-    //         }
-
-    //         INR_BALANCES[body.userId].balance -= body.price
-    //         INR_BALANCES[body.userId].locked += body.price
-    //         res.json(ORDERBOOK)
-    //         return
-    //     }
-
-    //     if(orderBookData[body.price].total === body.quantity){
-
-    //         // bought stocks for some money
-    //         INR_BALANCES[body.userId].locked -= totalAmount
-    //         STOCK_BALANCES[body.userId] = {
-    //             [body.stockSymbol] : {
-    //                 [stockType] : {
-    //                     quantity : 0,
-    //                     locked : 0
-    //                 }
-    //             }
-    //         }
-
-    //         // sold stocks for same money and quantity
-    //         const userIdList = Object.keys(orderBookData[body.price].orders)
-            
-    //         userIdList.map((userId)=>{
-    //             const userQuantity : number = ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId]
-    //             if(userId.includes("probo")){
-    //                 const reverseOrderId = userId.slice(6 , userId.length + 1)
-    //                 if(stockType === "yes"){
-                        
-    //                     STOCK_BALANCES[body.userId][body.stockSymbol]["yes"].quantity += body.quantity
-    //                     STOCK_BALANCES[reverseOrderId][body.stockSymbol]["no"].quantity += body.quantity
-    //                     INR_BALANCES[body.userId].balance -= body.price
-    //                     INR_BALANCES[reverseOrderId].balance -= 1000 - body.price
-
-    //                 } else if (stockType === "no") {
-    //                     STOCK_BALANCES[body.userId][body.stockSymbol]["no"].quantity += body.quantity
-    //                     STOCK_BALANCES[reverseOrderId][body.stockSymbol]["yes"].quantity += body.quantity
-    //                     INR_BALANCES[body.userId].balance -= body.price
-    //                     INR_BALANCES[reverseOrderId].balance -= (1000 - body.price)
-    //                 }
-    //             } else {
-    //                 INR_BALANCES[userId] = {
-    //                     balance : INR_BALANCES[userId].balance +userQuantity*body.price ,
-    //                     locked : INR_BALANCES[userId].locked - userQuantity*body.price
-    //                 }
-    //                 STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity += userQuantity
-    //                 STOCK_BALANCES[userId][body.stockSymbol][stockType].locked -= userQuantity
-    //             }
-                
-
-    //             delete ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId]
-    //         })
-    //         ORDERBOOK[body.stockSymbol][stockType][body.price].total -=  body.quantity
-
-    //     }
-
-    //     if(orderBookData[body.price].total > body.quantity){
-    //         // var totalQuantity : number = Number(body.quantity)
-    //         const userIdArray = Object.keys(ORDERBOOK[body.stockSymbol][stockType][body.price].orders)
-    //         var noOfStocksBought : number = 0;
-    //         var balancedNumber : number 
-    //         userIdArray.map((userId)=>{
-    //             balancedNumber = ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId] - (body.quantity - noOfStocksBought)
-    //             if(balancedNumber <= 0){
-    //                 INR_BALANCES[userId].balance = ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId] * body.price
-    //                 STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity += ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId]
-    //                 STOCK_BALANCES[body.userId][body.stockSymbol][stockType].locked -= body.quantity
-    //                 noOfStocksBought = noOfStocksBought + ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId]
-    //                 delete ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId]
-    //             }
-
-    //             if(balancedNumber > 0){
-    //                 INR_BALANCES[userId].balance = (ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId] - balancedNumber) * body.price
-    //                 STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity += body.quantity
-    //                 STOCK_BALANCES[body.userId][body.stockSymbol][stockType].locked -= body.quantity
-    //                 noOfStocksBought = noOfStocksBought + (ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId] -balancedNumber)
-    //                 ORDERBOOK[body.stockSymbol][stockType][body.price].orders[userId] = balancedNumber
-    //             }
-    //         })
-
-    //         ORDERBOOK[body.stockSymbol][stockType][body.price].total -= body.quantity
-
-    //         res.json(ORDERBOOK)
-    //         return           
-    //     }
-
-    //     // console.log(orderBookData[body.price])
-        
-        
-        
-    //     // case 2: have to mint partial amount of req stocks
-    //     if(orderBookData[body.price].total < body.quantity){
-    //         const remainingStocks = body.quantity - orderBookData[body.price].total
-    //         STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity += (body.quantity - remainingStocks)
-    //         INR_BALANCES[body.userId].locked -= orderBookData[body.price].total*body.price
-            
-    //         if(remainingStocks > 0) {
-    //             ORDERBOOK
-    //         }
-    //     }
-
-    //     // if(orderBookData[body.price].total < body.quantity){
-
-    //     // }
-
-    //     if(ORDERBOOK[body.stockSymbol][stockType][body.price].total === 0){
-    //         delete ORDERBOOK[body.stockSymbol][stockType][body.price]
-    //     }
-
-    //     res.json({
-    //         inrBalance : INR_BALANCES[body.userId],
-    //         stocksData : STOCK_BALANCES[body.userId]
-    //     })
-        
-
-
-        // if(ORDERBOOK[body.stockSymbol][stockType][body.price].total === 0){
-        //     if(stockType === "yes"){
-        //         lockedAmount = body.price * body.quantity + lockedAmount
-        //         const noPriceQuantity = body.price*body.quantity - 10000
-
-        //         const noINR = noPriceQuantity/body.quantity
-        //         // same no of no stock is generated as of required no. of yes stocks
-
-        //         ORDERBOOK[body.stockSymbol]["no"] = {
-        //             [noINR] : {
-        //                 total : body.quantity,
-        //                 orders : {
-        //                     ["probo"] : body.quantity
-        //                 }
-        //             }
-        //         }
-        //         if(lockedAmount === 10000){
-        //             STOCK_BALANCES[body.userId][body.stockSymbol]
-        //         }
-
-        //     } else if(stockType === "no"){
-        //         const noPriceQuantity = body.price*body.quantity - 10000
-        //         lockedAmount = body.price * body.quantity + lockedAmount
-        //         const noINR = noPriceQuantity/body.quantity
-        //         // same no of no stock is generated as of required no. of yes stocks
-
-        //         ORDERBOOK[body.stockSymbol]["yes"] = {
-        //             [noINR] : {
-        //                 total : body.quantity,
-        //                 orders : {
-        //                     ["probo"] : body.quantity
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     if(lockedAmount === 1000){
-
-        //     }
-
-        // }
     
-    // } else {
-    //     res.json("not enough balance")
-        
-    // }
-
-    // res.json("something went wrong")
     if(body.quantity*body.price <= INR_BALANCES[body.userId].balance){
         LockINR(body.userId , body.quantity*body.price)
         reqBuy(body.stockSymbol , body.quantity , body.price , body.userId , stockType)
@@ -432,63 +231,7 @@ app.post("/order/sell" , async (req : Request , res : Response)=>{
     const body = await req.body
     const stockType : "yes" | "no" = body.stockType
     try {
-    // const stockQuantity = STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity
-    // const currentPriceData = ORDERBOOK[body.stockSymbol][stockType][body.price]
     
-    // if(stockQuantity >= body.quantity){
-    //     STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity = stockQuantity - body.quantity
-    //     STOCK_BALANCES[body.userId][body.stockSymbol][stockType].locked += body.quantity
-    //     if(currentPriceData){
-    //         const currentQuantity = ORDERBOOK[body.stockSymbol][stockType][body.price].orders[body.userId]
-    //         if(currentQuantity){
-    //             ORDERBOOK[body.stockSymbol][stockType][body.price] = {
-    //                 total : currentPriceData.total + body.quantity,
-    //                 orders : {
-    //                     ...currentPriceData.orders,
-    //                     [body.userId] : body.quantity + currentQuantity
-    //                 }
-    //             }
-    //         }else {
-
-    //             ORDERBOOK[body.stockSymbol][stockType][body.price] = {
-    //                 total : currentPriceData.total + body.quantity,
-    //                 orders : {
-    //                     ...currentPriceData.orders,
-    //                     [body.userId] : body.quantity
-    //                 }
-    //             } 
-    //         }
-    //     }
-    //     else if(!ORDERBOOK[body.stockSymbol][stockType][body.price]){
-    //         ORDERBOOK[body.stockSymbol][stockType][body.price] = {
-    //                 total : body.quantity,
-    //                 orders : {
-    //                     [body.userId] : body.quantity
-    //                 }
-    //             }
-    //     } 
-
-        // console.log(ORDERBOOK[body.stockSymbol][stockType][body.price].orders[body.userId])
-        // else if (ORDERBOOK[body.stockSymbol][stockType][body.price].orders[body.userId]){
-        //     const currentQuantity = ORDERBOOK[body.stockSymbol][stockType][body.price].orders[body.userId]
-        //     ORDERBOOK[body.stockSymbol][stockType][body.price].total += body.quantity
-        //     ORDERBOOK[body.stockSymbol][stockType][body.price].orders[body.userId]
-        //     console.log(currentQuantity)
-        // }
-        // else if(!ORDERBOOK[body.stockSymbol][stockType]){
-        //     ORDERBOOK[body.stockSymbol][stockType] = {
-        //         [body.price] : {
-        //             total : body.stockQuantity,
-        //             orders : {
-        //                 [body.userId] : body.stockQuantity
-        //             }
-        //         }
-        //     }
-        // }
-    // } else {
-    //     res.json("you don't have enough stocks")
-    //     return
-    // }
     if(STOCK_BALANCES[body.userId][body.stockSymbol][stockType].quantity >= body.quantity){
 
         LockStock(body.userId , body.quantity , stockType , body.stockSymbol);
@@ -573,7 +316,6 @@ async function AddToOrderBook (
                 }
         }
 
-        // const changeOrderbook = ORDERBOOK[stockSymbol]
         await client.lPush("stocks", JSON.stringify({stock : ORDERBOOK[stockSymbol] , Symbol : stockSymbol}));
 
         console.log("orderbook changed and pushed to redis")
@@ -631,12 +373,10 @@ async function reqBuy(
     }
     
     if(ORDERBOOK[stockSymbol][stockType][price].total >= quantity || ORDERBOOK[stockSymbol][stockType][price].total < quantity){
-        // const userIdArray = ORDERBOOK[stockSymbol][stockType][price].orders
         const userIdArray = Object.keys(ORDERBOOK[stockSymbol][stockType][price].orders)
         var noOfStocksBought : number = 0;
         var balancedNumber : number = 0;
         userIdArray.map((userId)=>{
-            // ORDERBOOK[stockSymbol][stockType][price].orders[userId]
             
 
             balancedNumber = ORDERBOOK[stockSymbol][stockType][price].orders[userId] - (quantity - noOfStocksBought)
@@ -665,7 +405,6 @@ async function reqBuy(
                     noOfStocksBought = noOfStocksBought + (ORDERBOOK[stockSymbol][stockType][price].orders[userId] -balancedNumber)
                     ORDERBOOK[stockSymbol][stockType][price].orders[userId] = balancedNumber
                 }
-                // ORDERBOOK[stockSymbol][stockType][price].total = balancedNumber
             }
         })
         if(balancedNumber < 0){
@@ -698,7 +437,6 @@ async function reqBuy(
     }
 
     
-    // ORDERBOOK[stockSymbol][stockType][price]
 }
 
 
